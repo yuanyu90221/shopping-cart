2eb0a14ab5d91824636b6960a31dc8d270505723
yuanyu90221 <yuanyu90221@gmail.com>
2016-11-29 16:14:16 +0800

    modify all the login utility

--- app.js
+++ app.js
@@ -39,6 +39,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next){
+  res.locals.login = req.isAuthenticated();
+  next();
+});
 app.use('/user', userRoutes);
 app.use('/', routes);
 // catch 404 and forward to error handler
--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,17 @@ var csrfProtection = csrf();
 var passport = require('passport');
 // use csrfProtection
 router.use(csrfProtection);
+router.get('/profile', isLoggedIn , function(req, res, next){
+  res.render('user/profile');
+});
+router.get('/logout',isLoggedIn, function(req, res, next){
+    req.logout();
+    res.redirect('/');
+});
+
+router.use('/', notLoggedIn, function(req, res, next){
+    next();
+});
 router.get('/signup', function(req, res, next){
   var messages = req.flash('error');
   res.render('user/signup',{csrfToken: req.csrfToken(), messages:messages, hasErrors:messages.length > 0});
@@ -16,10 +27,6 @@ router.post('/signup', passport.authenticate('local.signup',{
   failureRedirect: '/user/signup',
   failureFlash: true
 }));
-
-router.get('/profile', function(req, res, next){
-  res.render('user/profile');
-});
 
 router.get('/signin', function(req, res, next){
  var messages = req.flash('error');
@@ -32,3 +39,17 @@ router.post('/signin', passport.authenticate('local.signin',{
   failureFlash: true
 }));
 module.exports = router;
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+}
+
+function notLoggedIn(req, res, next){
+    if(!req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+}
